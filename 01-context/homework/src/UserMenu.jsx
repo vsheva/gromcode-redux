0a78a507d46e63c4d8313f147bbab5e98924428b
vsheva <valera.sheva@gmail.com>
2022-06-13 23:41:10 +0300

    fix

--- 01-context/homework/src/UserMenu.jsx
+++ 01-context/homework/src/UserMenu.jsx
@@ -5,12 +5,12 @@ class UserMenu extends React.Component {
   render() {
     return (
       <div className="menu">
-        <span className="menu__greeting">Hello, {this.context.name}</span>
+        <span className="menu__greeting">{`Hello, ${this.context.name}`}</span>
         <img alt="User Avatar" src={this.context.avatar_url} className="menu__avatar" />
       </div>
     );
   }
 }
-UserMenu.propTypes = Context
+UserMenu.contextType = Context;
 export default UserMenu;
 
