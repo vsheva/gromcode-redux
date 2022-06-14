69edc57660c238b551bd69a787411877fbdfead8
vsheva <valera.sheva@gmail.com>
2022-06-14 15:17:25 +0300

    fix

--- 01-context/homework/src/App.jsx
+++ 01-context/homework/src/App.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import Header from './Header.jsx';
-import {userData, Context} from "./userData-context.js"
-
+import { Context, userData } from './userData-context.js';
 
 class App extends React.Component {
   state = {
     userData,
   };
 
-
   render() {
     return (
-        <div className="page">
-         <Context.Provider value={this.state.userData}>
+      <div className="page">
+        <Context.Provider value={this.state.userData}>
           <Header />
-         </Context.Provider>
-        </div>
+        </Context.Provider>
+      </div>
     );
   }
 }
 
 export default App;
+
--- 01-context/homework/src/userData-context.js
+++ 01-context/homework/src/userData-context.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 export const userData = {
-  name: "Nikola Tesla",
-  avatar_url: "https://avatars3.githubusercontent.com/u10001",
+  name: 'Nikola Tesla',
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
 };
 
-export const Context = React.createContext({userData});
+export const Context = React.createContext(userData);
+
+
+//export const Context = React.createContext({});
