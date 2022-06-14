ad671b52810d06e870021f0c6a2cbbed224f9f2c
vsheva <valera.sheva@gmail.com>
2022-06-14 21:49:54 +0300

    fix

--- 02-redux-intro/task-01/src/index.js
+++ 02-redux-intro/task-01/src/index.js
@@ -3,31 +3,74 @@ import {createStore} from 'redux';
 const INCREMENT ='COUNTER/INCREMENT';
 const DECREMENT ='COUNTER/DECREMENT';
 
- const increment=()=>{
-return {
-       type: INCREMENT,
-       };
+export const increment=()=>{
+    return {
+        type: INCREMENT,
+    };
 }
 
-const decrement=()=>{
-   return {
+export const decrement=()=>{
+    return {
         type: DECREMENT,
     };
 };
 
 
-export const counterReducer = (state=0, action)=> {
-switch (action.type) {
-    case INCREMENT:
-         return state+1;
-    case DECREMENT:
-         return state-1;
-    default:
-          return state;
-}
+export const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state;
+    }
 };
 
 export const store = createStore(counterReducer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {createStore} from 'redux';
+//
+// const INCREMENT ='COUNTER/INCREMENT';
+// const DECREMENT ='COUNTER/DECREMENT';
+//
+//  const increment=()=>{
+// return {
+//        type: INCREMENT,
+//        };
+// }
+//
+// const decrement=()=>{
+//    return {
+//         type: DECREMENT,
+//     };
+// };
+//
+//
+// export const counterReducer = (state=0, action)=> {
+// switch (action.type) {
+//     case INCREMENT:
+//          return state+1;
+//     case DECREMENT:
+//          return state-1;
+//     default:
+//           return state;
+// }
+// };
+//
+// export const store = createStore(counterReducer);
 
 // store.dispatch(increment);
 // store.dispatch(increment);
