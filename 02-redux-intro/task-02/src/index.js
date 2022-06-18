95905d30968cff96eb7ddb90eda0945ce8983b04
vsheva <valera.sheva@gmail.com>
2022-06-18 22:30:53 +0300

    fix

--- 02-redux-intro/task-02/src/index.js
+++ 02-redux-intro/task-02/src/index.js
@@ -24,8 +24,7 @@ resetBtn.addEventListener('click', onReset);
 
 store.subscribe(() => {
   const state = store.getState();
-  const currentValue = state.history.reduce((acc, value) => +acc + +value, 0);
+  const currentValue = state.history.reduce((acc, num) => acc + +num, 0);
   const historyString = state.history.join('');
-
-  resultElem.textContent = state.history.length === 0 ? '' : `${historyString}=${currentValue}`;
+  resultElem.textContent = !state.history.length ? '' : `${historyString} = ${currentValue}`
 });
