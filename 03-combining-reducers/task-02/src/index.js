1259da3323661bf905e77ccfe60a3b482370a21e
vsheva <valera.sheva@gmail.com>
2022-06-27 22:22:29 +0300

    fix

--- 03-combining-reducers/task-02/src/index.js
+++ 03-combining-reducers/task-02/src/index.js
@@ -1,44 +1,15 @@
-import { createStore } from 'redux';
+import store from './store';
+import { increment, decrement } from './counter';
 
-//1.action type
-const INCREMENT = 'COUNTER/INCREMENT';
-const DECREMENT = 'COUNTER/DECREMENT';
+store.subscribe(() => {
+    console.log(store.getState());
+});
 
-//actions
- const increment = () => {
-  return {
-    type: INCREMENT,
-  };
-};
-
- const decrement = () => {
-  return {
-    type: DECREMENT,
-  };
-};
-
- //2. reducer(state, action) => new state ({})
-//action.type - case INCREMENT => return STATE
- const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return state + 1;
-    case DECREMENT:
-      return state - 1;
-    default:
-      return state;
-  }
-};
- //3.store(reducer)
-
- const store = createStore(counterReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
- //method store.dispatch(action function) -обновление состояния
+//method store.dispatch(action function) -обновление состояния
 store.dispatch(increment());
 store.dispatch(increment());
 store.dispatch(increment());
 store.dispatch(increment());
 store.dispatch(decrement());
 
-//method
-console.log(store.getState());
+
