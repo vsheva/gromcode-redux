52f786890e2a30b5ebb8508432483d2b25941608
vsheva <valera.sheva@gmail.com>
2022-06-27 21:48:50 +0300

    fix

--- 03-combining-reducers/task-01/src/users.reducer.js
+++ 03-combining-reducers/task-01/src/users.reducer.js
@@ -1,3 +1,61 @@
+import {ADD_USER, DELETE_USER, UPDATE_USER} from "./users.actions"
+/*
+начальное состояние - это обьект {userlist: [] }
+
+algo:
+---строчка 12 - вернуть старое состояние (деструктурированное) + указать интересующее нас свойство
+---строчка 13, указать интересующее нас свойство  userList, которое равно
+--- старому usersList + к которому добавляю данные из обьекта action - action.payload.userData
+    используем немутирующий метод массивов concat()
+    */
+
+const usersReducer = (state = {userList: []}, action) => {
+    switch (action.type) {
+        case ADD_USER:
+            return {
+                ...state,
+                userList: state.userList.concat(action.payload.userData)
+            };
+
+        /* algo
+        -убрать пользователя из массива по его id
+        -пройдусь по существующим пользователям и проверю, что
+        у этого пользователя user.id (каждый) не равен тому id,
+        которое пришло в action из payload
+        -filter создаст новый список ( новое состояние с новым списком)*/
+        case DELETE_USER:
+            return {
+                ...state,
+                userList: state.userList.filter(user => user.id !== action.payload.userId)
+            };
+
+        //  algo: 1)создал обьект, 2)заполнил старыми данными 3)добавил изменения, которые пришли по action
+        case UPDATE_USER:
+            return {
+                ...state,
+                userList: state.userList.map(user => {
+                        if (user.id !== action.payload.userId) {
+                            return {...user, ...action.payload.userData};
+                        }
+                        return user;
+                    })
+            };
+
+
+        default:
+            return state;
+    }
+}
+
+export default usersReducer;
+
+
+
+
+
+// with payload
+
+/*
 import { ADD_USER, DELETE_USER, UPDATE_USER } from './users.actions';
 
 const initialState = {
@@ -38,78 +96,11 @@ const initialState = {
 
 
 export default usersReducer
-
-
-
-
-
-
-
-/*
-import {ADD_USER, DELETE_USER, UPDATE_USER} from "./users.actions"
-
-
-//начальное состояние - это обьект {userlist: [] }
-//---строчка 12 - вернуть старое состояние (деструктурированное) + указать интересующее нас свойство (ИТОГ: ВЕРНУТЬ ОБЬЕКТ с НОВЫМ СОСТОЯНИЕМ)
-//---строчка 13, указать интересующее нас свойство  userList, которое равно
-//--- старому usersList + к которому добавляю данные из обьекта action - action.payload.userData
-//     используем немутирующий метод массивов concat()
-
-const usersReducer = (state = {userList: []}, action) => {
-    switch (action.type) {
-        case ADD_USER:
-            return {
-                ...state,
-                userList: state.userList.concat(action.payload.userData)
-            };
-
-        //убрать пользователя из массива по его id
-        //пройдусь по существующим пользователям и проверю, что
-        //у этого пользователя user.id (каждый) не равен тому id,
-        // которое пришло в action из payload
-
-        //filter создаст новый список ( новое состояние с новым списком)
-        case DELETE_USER:
-            return {
-                ...state,
-                userList: state.userList.filter((user) =>
-                    user.id !== action.payload.userId)
-            };
-
-        case UPDATE_USER:
-            const newList = state.usersList.map(user => {
-                if (user.id === action.payload.userId) {
-                    return {
-                        ...user,
-                        ...action.payload.userData,
-                    };
-                }
-                return user;
-            });
-
-
-
-            return {
-                ...state,
-                userList: newList,
-
-                /!*state.userList.map((user) => {
-                        if (user.id !== action.payload.userId) {
-                            return { //создал обьект
-                                ...user, //заполнил старыми данными
-                                ...action.payload.userData, //добавил изменения, которые пришли по action
-                            };
-                        }
-                        return user;
-                    })*!/
-            };
-
-
-        default:
-            return state;
-    }
-}
-
-export default usersReducer;
-
 */
+
+
+
+
+
+
+
