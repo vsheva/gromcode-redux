77c2ea7fcb0d1b5e3fb760ec2fa74a1b7d2e2398
vsheva <valera.sheva@gmail.com>
2022-06-30 23:37:58 +0300

    fix

--- 04-react-redux/home/src/UsersList.jsx
+++ 04-react-redux/home/src/UsersList.jsx
@@ -12,7 +12,7 @@
 // ~  FIX
 
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as userActions from './users/users.actions';
 
 import Pagination from './Pagination.jsx';
@@ -22,59 +22,59 @@ import User from './User.jsx';
 //currentPage
 
 class UsersList extends React.Component {
-  state = {currentPage: 2};
-  itemsPerPage = 3;
+    state = {currentPage: 2};
+    itemsPerPage = 3;
 
-  prevButtonHandler = () => {
-    this.setState({
-      currentPage: this.state.currentPage - 1,
-    });
-  };
+    prevButtonHandler = () => {
+        this.setState({
+            currentPage: this.state.currentPage - 1,
+        });
+    };
 
-  nextButtonHandler = () => {
-    this.setState({
-      currentPage: this.state.currentPage + 1,
-    });
-  };
+    nextButtonHandler = () => {
+        this.setState({
+            currentPage: this.state.currentPage + 1,
+        });
+    };
 
-  render() {
-    const users = this.props.users;
+    render() {
+        const users = this.props.users;
 
-    const startIndex = (this.state.currentPage - 1) * this.itemsPerPage;//*
-    console.log(startIndex)
-    const endIndex = startIndex + this.itemsPerPage;
-    const usersToRender = users.slice(startIndex, endIndex);
+        const startIndex = (this.state.currentPage - 1) * this.itemsPerPage;//*
+        console.log(startIndex)
+        const endIndex = startIndex + this.itemsPerPage;
+        const usersToRender = users.slice(startIndex, endIndex);
 
-    return (
-      <div>
-        <Pagination
-          currentPage={this.state.currentPage}
-          goPrev={this.prevButtonHandler}
-          goNext={this.nextButtonHandler}
-          itemsPerPage={this.state.itemsPerPage}
-          totalItems={users.length}
-        />
+        return (
+            <div>
+                <Pagination
+                    currentPage={this.state.currentPage}
+                    goPrev={this.prevButtonHandler}
+                    goNext={this.nextButtonHandler}
+                    itemsPerPage={this.state.itemsPerPage}
+                    totalItems={users.length}
+                />
 
-        <ul className="users">
-          {usersToRender.map(user => (
-            <User key={user.id} name={user.name} age={user.age}/>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+                <ul className="users">
+                    {usersToRender.map(user => (
+                        <User key={user.id} name={user.name} age={user.age}/>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 
 const mapState = (state) => {
-  return {
-    users: state.users.usersList, //обращаемся к стору
-  };
+    return {
+        users: state.users.usersList, //обращаемся к стору
+    };
 };
 
 const mapDispatch = {
-  createUser: userActions.addUser,  //отправляем в UI
-  deleteUser: userActions.deleteUser, //достук к action, к-ым буду создавать пользователя
+    createUser: userActions.addUser,  //отправляем в UI
+    deleteUser: userActions.deleteUser, //достук к action, к-ым буду создавать пользователя
 };
 
 
