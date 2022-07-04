aafbb93df56da63d6f1e4cc60e22dfe5a5e710de
vsheva <valera.sheva@gmail.com>
2022-07-04 12:43:20 +0300

    fix

--- 04-react-redux/home/src/Pagination.jsx
+++ 04-react-redux/home/src/Pagination.jsx
@@ -6,11 +6,11 @@ const Pagination = ({ goPrev, goNext, itemsPerPage, totalItems, currentPage }) =
 
   return (
     <div className="pagination">
-      <button disabled={isPrevPageAvailable} onClick={() => goPrev()} className="btn">
+      <button disabled={!isPrevPageAvailable} onClick={() => goPrev()} className="btn">
         {isPrevPageAvailable ? null : '←'}
       </button>
       <span className="pagination__page">{currentPage}</span>
-      <button disabled={isNextPageAvailable} onClick={() => goNext()} className="btn">
+      <button disabled={!isNextPageAvailable} onClick={() => goNext()} className="btn">
         {isNextPageAvailable ? null : '→'}
       </button>
     </div>
