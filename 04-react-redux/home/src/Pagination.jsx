501eaa83c1748781006dd6ac7945033e421f75df
vsheva <valera.sheva@gmail.com>
2022-07-04 15:21:07 +0300

    fix

--- 04-react-redux/home/src/Pagination.jsx
+++ 04-react-redux/home/src/Pagination.jsx
@@ -7,11 +7,12 @@ const Pagination = ({ goPrev, goNext, itemsPerPage, totalItems, currentPage }) =
   return (
     <div className="pagination">
       <button disabled={!isPrevPageAvailable} onClick={goPrev} className="btn">
-        {isPrevPageAvailable && '←'}
+         {isPrevPageAvailable && '←'}
+
       </button>
       <span className="pagination__page">{currentPage}</span>
       <button disabled={!isNextPageAvailable} onClick={goNext} className="btn">
-        {isNextPageAvailable && '→'}
+          {isNextPageAvailable && '→'}
       </button>
     </div>
   );
@@ -19,10 +20,25 @@ const Pagination = ({ goPrev, goNext, itemsPerPage, totalItems, currentPage }) =
 
 export default Pagination;
 
-/*
-<button disabled={!isPrevPageAvailable} onClick={() => goPrev()} className="btn">
-    {isPrevPageAvailable && '←'}
-</button>
-<span className="pagination__page">{currentPage}</span>
-<button disabled={!isNextPageAvailable} onClick={() => goNext()} className="btn">
-    {isNextPageAvailable && '→'}*/
+
+/* old version
+const Pagination = ({ goPrev, goNext, itemsPerPage, totalItems, currentPage }) => {
+    const isPrevPageAvailable = currentPage > 1;
+    const isNextPageAvailable = currentPage < Math.ceil(totalItems / itemsPerPage);
+
+    return (
+        <div className="pagination">
+            <button disabled={!isPrevPageAvailable} onClick={()=>goPrev()} className="btn">
+                {isPrevPageAvailable ? '←' : false}
+
+            </button>
+            <span className="pagination__page">{currentPage}</span>
+            <button disabled={!isNextPageAvailable} onClick={()=>goNext()} className="btn">
+                {isNextPageAvailable ? '→' : false}
+            </button>
+        </div>
+    );
+};
+
+export default Pagination;
+*/
