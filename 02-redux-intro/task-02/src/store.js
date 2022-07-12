9ae8b103dedb5a18dd6a03ab4612e67c85a83e2d
vsheva <valera.sheva@gmail.com>
2022-07-12 20:34:49 +0300

    fix

--- 02-redux-intro/task-02/src/store.js
+++ 02-redux-intro/task-02/src/store.js
@@ -1,61 +1,50 @@
 import { createStore } from 'redux';
 
-//actions
 const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
 const RESET = 'COUNTER/RESET';
 
 export const increment = () => {
-  return {
-    type: INCREMENT,
-  };
+  return { type: INCREMENT };
 };
 
 export const decrement = () => {
-  return {
-    type: DECREMENT,
-  };
+  return { type: DECREMENT };
 };
 
 export const reset = () => {
-  return {
-    type: RESET,
-  };
+  return { type: RESET };
 };
 
-//reducer
-
-/*const initialState = {
-  //value: 0,
+const initialState = {
+  value: 0,
   history: [],
-};*/
+};
 
-const counterReducer = (state = { history: [] }, action) => {
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return {
         ...state,
-        //value: state.value + 1,
+        value: state.value + 1,
         history: state.history.concat('+1'),
       };
     case DECREMENT:
       return {
         ...state,
-        //value: state.value - 1,
-        history: state.history.concat(-1),
+        value: state.value - 1,
+        history: state.history.concat('-1'),
       };
     case RESET:
       return {
         ...state,
-        //value: 0,
+        value: 0,
         history: [],
       };
     default:
       return state;
   }
 };
-
-//state
 
 const store = createStore(counterReducer);
 
